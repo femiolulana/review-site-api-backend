ee3ae6e651993a51ee7a909a7078cdcd7da7fde2
femiolulana <oniseikeji@gmail.com>
2024-07-24 01:42:37 -0400

    anothe one

--- api/reviews.route.js
+++ api/reviews.route.js
@@ -2,4 +2,6 @@ import express from "express"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.se)
+router.route("/").get((req, res) => res.send("hello world"))
+
+export default router
